2c65302c5f8879844b6a00128315ef276a46d570
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2023-12-10 00:04:02 -0300

    project finished

---
+++ pipes/pipes.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+
+@onready var score_sound = $ScoreSound
+
+const SCROLL_SPEED: float = 120.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.x -= SCROLL_SPEED * delta
+
+func player_scored() -> void:
+	score_sound.play()
+	GameManager.increment_score()
+
+
+func _on_screen_exited():
+	queue_free()
+
+
+func _on_pipe_body_entered(body):
+	if body.is_in_group(GameManager.GROUP_PLANE):
+		body.die()
+
+func _on_laser_body_entered(body):
+	if body.is_in_group(GameManager.GROUP_PLANE):
+		player_scored()
